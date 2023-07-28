6746e031026987c1e71d7aefed8a73c45a9c522a
Сергей Ангелов <sergejangelov@MacBook-Air-Sergej.local>
2023-07-28 13:18:58 +0300

    HomeWork3

--- HWs.playground/Contents.swift
+++ HWs.playground/Contents.swift
@@ -1,76 +1,99 @@
 import UIKit
 
-/* Псевдоним типа(typealias) позволяет задать альтернативное имя для существующих типов данных для повышения читабельности кода */
+let multilineString = """
 
-typealias name = String
-typealias age = Int
-typealias russian = Bool
-typealias weight = Double
-
-let myName: name = "Sergey"
-let myAge: age = 27
-let myNation: russian = true
-let myWeight: weight = 111.1
-
-
-let a = 21
-let b = 2.5
-let c = 11.07
-
-let sumOne = Double(a) + b + c
-
-print(sumOne)
-
-let sumTwo = a - Int(b) - Int(c)
-
-print(sumTwo)
-
-let sumThree = Double(a) / b + c
-
-print(Int(sumThree))
-
-
-/* Целое число(Int) - тип данных обозначающий целое число */
-
-
-/* Числовые литералы - числа в необработанном виде*/
-
-let exampleOne = 255
-let exampleTwo = 0b11111111
-let exampleThree = 0xFF
-let exampleFour = 0o377
-
-
-/* Int8, UInt8, Int16, UInt16, Int32, UInt32, Int64, UInt64 */
-
-
-/* Unicod - интернациональный стандарт для кодирования */
-
-let questio: Character = "\u{0049}"
-let eAcute: Character = "\u{2665}"
-let combinedEAcute: Character = "\u{24}"
-
-
-/* Hashable - протокол который предоставляет hashValue объекту, который можно поместить в хеш - таблицу, для эффективного поиска и сопоставления данных.
+ Скажи-ка, дядя, ведь недаром
+ Москва, спаленная пожаром,
+ Французу отдана?
+ Ведь были ж схватки боевые,
+ Да, говорят, еще какие!
+ Недаром помнит вся Россия
+ Про день Бородина!
  
-  Comparable - типы данных которые, могут быть сравнены между собой для определения отношений порядка */
+ """
 
 
-struct Employee: Hashable {
-    
-    var name: String
-    
+var name = "Sergey"
+
+for i in name {
+    print(i)
 }
 
-let objectOne = Employee(name: "Sarah")
-let objectTwo = Employee(name: "Sarah")
-let objectThree = Employee(name: "John")
 
-objectOne.hashValue
-objectTwo.hashValue
-objectThree.hashValue
+name += " Angelov"
 
-/* Хеш - целое число, которое зависит от используемой системы. В зависимости от этого можно получить разные числа одного и того же кода  */
+let age = 27
+
+print("Hi! My name is \(name). I'm \(age) years old.")
 
 
-/* Основное различие между Float и Double заключается в том, что Float хранит в себе 32 битные данные с плавающей точкой, в то время как Double 64  битные. Точность Double = 15 цифр, точность Float = 7 цифр. Apple рекомендует использовать Double.  */
+let greetigs = "Hello there!"
+
+var secondGreetings = ""
+
+for char in greetigs {
+    if char == " " {
+        continue
+    }
+    secondGreetings.append(char)
+}
+
+print(secondGreetings)
+
+
+let reversed = String(greetigs.reversed())
+
+print(reversed)
+
+
+if greetigs.contains("o") {
+    print("Yes")
+} else {
+    print("No")
+}
+
+
+let time = "23:51:01"
+
+let elements = time.components(separatedBy: ":")
+
+for element in elements {
+    print(Int(element) ?? 0)
+}
+
+var elementOne = ""
+var elementTwo = ""
+var elementThree = ""
+
+var counter = 0
+
+for char in time {
+    if String(char) != ":" {
+        switch counter {
+        case 0: elementOne += String(char)
+        case 1: elementTwo += String(char)
+        default: elementThree += String(char)
+        }
+    } else {
+        counter += 1
+    }
+}
+
+Int(elementOne)
+Int(elementTwo)
+Int(elementThree)
+
+
+let pi = 3.14159
+let formatedPi = String(format: "%.2f", pi)
+
+
+var letters = "AaBbCc"
+
+letters.uppercased()
+
+letters.removeFirst()
+
+letters.removeLast()
+
+letters.append("!")
