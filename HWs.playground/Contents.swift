357b4b2533926fcf245ea8115d132c01e817fa46
Сергей Ангелов <sergejangelov@MacBook-Air-Sergej.local>
2023-08-06 18:40:18 +0300

    HomeWork9

--- HWs.playground/Contents.swift
+++ HWs.playground/Contents.swift
@@ -2,194 +2,246 @@ import UIKit
 
 //1) Повторите то, что было в видео.
 
-//for i in 0...200 {
-//    if i == 10 {
-//        break
+/*
+ func имяФункции(принимаемые параметры) -> вовзращаемые параметры {
+ ... тело функции ...
+ }
+ */
+
+//func sayFunc() {
+//    print("Hi")
+//}
+//
+//sayFunc()
+//
+//var arrayJournal = [String]()
+//
+//func addStudent(name: String, profession: String) {
+//    arrayJournal.append(name)
+//    arrayJournal.append(profession)
+//
+//    print(arrayJournal)
+//}
+//
+//addStudent(name: "Jack", profession: "IOS Dev")
+//addStudent(name: "Ivan", profession: "Android Dev")
+//addStudent(name: "Jogn", profession: "Java Dev")
+//
+//func calculate(number1: Int, number2: Int) -> Int {
+//    return number1 + number2
+//}
+//
+//calculate(number1: 10, number2: 20)
+//
+//let sum = calculate(number1: 10, number2: 20)
+//
+//print(sum)
+
+//var money = [1, 1, 52, 10, 70, 46, 46, 5, 2, 4]
+//var money2 = [1, 32, 352, 130, 70, 46, 46, 5, 24, 34]
+//
+//func calcMoney(inArray: [Int]) -> Int {
+//    var sum = 0
+//    for num in inArray {
+//        sum += num
+//    }
+//    return sum
+//}
+//
+//calcMoney(inArray: money)
+//let sum = calcMoney(inArray: money2)
+//
+//print(sum)
+//
+//func generalFunc(answer: Bool) -> () -> String {
+//
+//    func sayYes() -> String {
+//        return "Yes"
 //    }
 //
-//    print(i)
+//    func sayNo() -> String {
+//        return "No"
+//    }
+//
+//    return answer ? sayYes : sayNo
 //}
 //
-//var day = 1
-//
-//if day == 1 {
-//    print("Будний день")
-//} else if day == 2 {
-//    print("Будний день")
-//} else if day == 3 {
-//    print("Будний день")
-//} else if day == 4 {
-//    print("Будний день")
-//} else if day == 5 {
-//    print("Будний день пятница!")
-//} else if day == 6 {
-//    print("Выходной")
-//} else if day == 7 {
-//    print("Выходной")
-//}
-//
-//switch day {
-//case 1...4:
-//    print("Будний день")
-//case 5:
-//    print("Будний день пятница!")
-//case 6...7:
-//    print("Выходной")
-//default:
-//    break
-//}
-
-//let nameWork = "Ios Dev"
-//let age = 51
-//let peopleSex = "M"
-//
-//switch (nameWork, age) {
-//case (_, 1...17):
-//    print("Ты маловат")
-//case ("Ios Dev", 18...50) where peopleSex == "W":
-//    print("Девушек не берем")
-//case ("Ios Dev", 18...50) where peopleSex == "M":
-//    print("Вы нам подходите")
-//case let(name, age):
-//    print("\(name) в \(age) год?")
-//
-//default:
-//    print("Введите корректные значения")
-//}
+//generalFunc(answer: true)()
 
 
-//2) Зайти обязательно и познакомиться с документацией. Выпишите около 5 примеров.
+//2) Зайти обязательно и познакомиться с документацией. Выпишите 5 примеров в плейграунд.
 
-let names = ["Anna", "Alex", "Brian", "Jack"]
-for name in names {
-    print("Hello, \(name)!")
+func greet(person: String) -> String {
+    let greeting = "Привет, " + person + "!"
+    return greeting
 }
 
-var productTemperature = 20
+print(greet(person: "Anna"))
 
-repeat {
-    productTemperature += 20
-    print(productTemperature)
-} while productTemperature < 60
 
-while productTemperature < 80 {
-    productTemperature += 20
-    print(productTemperature)
+func greetAgain(person: String) -> String {
+    return "Hello again, " + person + "!"
 }
 
+print(greetAgain(person: "Anna"))
 
-//3) Создать цикл в цикле. В первом цикле интервал 200 раз во втором как дойдем до 15 выйти со всех циклов сразу.
 
-for i in 0...200 {
-    if i == 15 {
-        break
+func sayHelloWorld() -> String {
+    return "hello, world"
+}
+print(sayHelloWorld())
+
+
+func greet(person: String, alreadyGreeted: Bool) -> String {
+    if alreadyGreeted {
+        return greetAgain(person: person)
+    } else {
+        return greet(person: person)
     }
-    print(i)
+}
+print(greet(person: "Tim", alreadyGreeted: true))
+
+
+func minMax(array: [Int]) -> (min: Int, max: Int) {
+    var currentMin = array[0]
+    var currentMax = array[0]
+    for value in array[1..<array.count] {
+        if value < currentMin {
+            currentMin = value
+        } else if value > currentMax {
+            currentMax = value
+        }
+    }
+    return (currentMin, currentMax)
+}
+
+let bounds = minMax(array: [8, -6, 2, 109, 3, 71])
+
+print("min is \(bounds.min) and max is \(bounds.max)")
+
+
+//3) Создать 4 функции:
+//Которая ничего не принимает и ничего не возвращает.
+
+func emptyFunc() {
+    print("Empty")
+}
+
+//Принимает параметры но не возвращает.
+
+func funcNumOne(name: String) {
+    print("Hello \(name)")
+}
+
+//Принимает параметры и возвращает.
+
+func funcNumTwo(name: String) -> String {
+    return "Hello" + " " + name + "!"
+}
+
+print(funcNumTwo(name: "Sergey"))
+
+//Не принимает параметры но возвращает тюпл из 3х параметров.
+
+func funcNumThree() -> (Int, String, Bool) {
+    return (1, "Hello", false)
+}
+
+let result = funcNumThree()
+
+
+//4) Создать журнал для учителя, который будет принимать имя студента , профессию и оценку и записывает это все в массив. И внесите 10 студентов туда и распечатаете через цикл for.
+
+var journal = [String]()
+
+func addStudent(name: String, prof: String, score: Int) {
+    journal.append(name)
+    journal.append(prof)
+    journal.append(String(score))
+}
+
+addStudent(name: "Sergey", prof: "Ios Dev", score: 5)
+addStudent(name: "Mark", prof: "Android Dev", score: 4)
+addStudent(name: "Andry", prof: "Java Dev", score: 3)
+addStudent(name: "Antony", prof: "Go Dev", score: 2)
+addStudent(name: "Jane", prof: "Web Dev", score: 1)
+addStudent(name: "Alice", prof: "Designer", score: 2)
+addStudent(name: "Vladimir", prof: "Dart Dev", score: 3)
+addStudent(name: "Alex", prof: "Flutter Dev", score: 4)
+addStudent(name: "Evgeniy", prof: "Ios Dev", score: 5)
+addStudent(name: "John", prof: "Backend Dev", score: 4)
+
+for student in journal {
+    print(student)
 }
 
 
-//4) Создать обработку возраста в switch, если человеку от 6 до 17 то выводить, что ему пора нужно в школу, если от 18 до 25, то в универ и так далее, аж до пенсии.
+//5) Создать функцию которая принимает имя и фамилию, потом положить это в массив и отсортировать и вывести результат в консоль.
 
-var age = 20
 
-switch age {
-case 0...5:
-    print("В садик")
-case 6...18:
-    print("В школу")
-case 19...24:
-    print("В университет")
-case 25...65:
-    print("На работу")
-case 66..<age:
-    print("На пенсию")
-default:
-    print("Введите корректный возраст")
+var arrayFullName = [String]()
+
+func nameSurname(name: String, surname: String) {
+    arrayFullName.append(name)
+    arrayFullName.append(surname)
+    print(arrayFullName.sorted())
+}
+          
+nameSurname(name: "Sergey", surname: "Angelov")
+nameSurname(name: "Inna", surname: "Pavlova")
+nameSurname(name: "Alex", surname: "Angelov")
+nameSurname(name: "Eva", surname: "Angelova")
+
+
+//6) Создать функцию которая принимает параметры и вычисляет площадь круга.
+
+func circleS(radius: Double) -> Double {
+    let pi = 3.14
+    return radius * radius * pi
 }
 
+let circle = circleS(radius: 3)
 
-//5) Сделать 4 е задание через if конструкцию.
 
-let ageMan = 25
+//7) Создать те пустой массив кошелек, создайте функцию которая принимает массив в параметрах и в теле функции добавляет в этот массив значение 100. Далее вызовите массив и потом распечатайте значение массива.
 
-if ageMan < 5 {
-    print("В садик")
-} else if ageMan >= 6 && ageMan <= 18 {
-    print("В школу")
-} else if ageMan >= 19 && age <= 24 {
-    print("В университет")
-} else if ageMan >= 25 && ageMan <= 65 {
-    print("На работу")
-} else if ageMan > 66 {
-    print("На пенсию")
-} else {
-    print("Введите корректный возраст")
+var wallet = [Int]()
+
+func balance(with amount: Int) {
+    wallet.append(amount)
 }
 
+balance(with: 100)
 
-/*
- 6) Создать switch, который принимает сразу точку по x и y например (1, 1) и написать условие если x,y по 0 то выводить x,y и написать, что это точка origin, если значение (1, 0), то выводим точку и говорим вы на оси x, (0, 1) вы на оси y, если x и y от -2 до 2 то писать вы внутри координат.
-*/
+print(wallet[0])
 
-let x = 1
-let y = 1
 
-switch (x, y) {
-case (0, 0):
-    print("\(x) : \(y) - точка Origin")
-case (1, 0):
-    print("\(x) : \(y) - вы на оси Х")
-case (0, 1):
-    print("\(x) : \(y) - вы на оси Y")
-case let coordinates where x >= -2 && x <= 2 && y >= -2 && y <= 2:
-    print("Вы внутри координат Х: \(coordinates.0) : \(coordinates.1)")
-default:
-    print("Неизвестные коорддинаты")
-    
+//8) Написать функцию в которой можно передавать в параметры каждый раз разное количество Int например иногда 1,5,9 а следующий раз 5,10, 11, 20, 50 функция все это посчитает и вернет сумму этих чисел.
+
+
+func sum(nums: [Int]) -> Int {
+    return nums.reduce(0, +)
 }
 
+let numbers = sum(nums: [1, 2, 3, 4, 5])
 
 
-//7) Написать switch в котором 3 кейса 1,2,3 с принтами этих индексов. Когда мы передаем 1 индекс печатать принт 1 го кейса, а когда передаем 2 й индекс то печатать сразу принт 2- го и 3-го кейса.
+//9) Написать любую функцию которая вернет throws.
 
-let number = 2
 
-switch number {
-case 1:
-    print(1)
-case 2:
-    print(2)
-    fallthrough
-case 3:
-    print(3)
-default:
-    break
+enum LoginErrors: Error {
+    case badLog
+    case badPass
 }
 
-
-//8) Придумать свой switch в котором будет 3 кейса и в одном из ник доп. условие с оператором where.
-
-let nameWork = "Ios Dev"
-let ageIosDev = 27
-let peopleSex = "M"
-
-switch age {
-case 1...17:
-    print("Ты маловат")
-case 18...50 where peopleSex == "W":
-    print("Девушек не берем")
-case 18...50:
-    print("Вы нам подходите")
-default:
-    print("Введите корректные значения")
+func errorThrows(log: String, pass: String) throws -> Bool {
+    if log.isEmpty { throw LoginErrors.badLog }
+    if pass.isEmpty { throw LoginErrors.badPass }
+    return true
 }
 
-
-//9) Создайте переменную с значением 10 и через цикл while печатайте свое имя пока значение не станет 0.
-
-var times = 10
-
-while times > 0 {
-print("Sergey")
-    times -= 1
+do {
+    try errorThrows(log: "Angelov", pass: "2571883")
+} catch {
+    print(error.localizedDescription)
 }
