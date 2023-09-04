3e5954c3e7277e7687860943e72612a30fd6193c
Сергей Ангелов <sergejangelov@MacBook-Air-Sergej.local>
2023-09-04 10:16:01 +0300

    HomeWork18

--- HWs.playground/Contents.swift
+++ HWs.playground/Contents.swift
@@ -1,437 +1,329 @@
 import UIKit
 
-//Automatic reference counting(автоматический подсчет ссылок) = ARC
+/*
+ 1) Прочитать документацию по уроку из:
+ https:// developer.apple.com
+ swiftbook.ru.
+ Выписать 5 примеров
+ */
 
-//1. Повторите то, что есть в видео.
-
-//class Person {
-//
-//    var name = String()
-//
-//    init(name: String) {
-//
-//        self.name = name
-//        print("\(name) инициализирован и создан в памяти")
-//    }
-//    deinit {
-//
-//        print("\(name) удален из памяти")
-//    }
+//final class Person {
+//    var residence: Residence?
 //}
 //
-//var ref1: Person?
-//var ref2: Person?
-//var ref3: Person?
-//
-//ref1 = Person(name: "Jack")
-//ref2 = ref1
-//ref3 = ref1
-//
-//ref1 = nil
-//ref2 = nil
-//ref3 = nil
-
-
-//------------------Цикл сильных ссылок
-
-//class Hotel {
-//
-//    var name = String()
-//
-//    init(name: String) {
-//        self.name = name
-//    }
-//
-//    var apart: Apartament?
-//
-//    deinit {
-//        print("\(name) выезжает")
-//    }
+//final class Residence {
+//    var numberOfRooms = 1
 //}
 //
-//class Apartament {
+//let john = Person()
 //
-//    var room = String()
-//
-//    init(room: String) {
-//        self.room = room
-//    }
-//
-//    weak var hotel: Hotel?
-//
-//    deinit {
-//        print("Апартаменты \(room) освобождаются")
-//    }
+//if let roomCount = john.residence?.numberOfRooms {
+//    print("John's residence has \(roomCount) room(s).")
+//} else {
+//    print("Unable to retrieve the numbers of rooms.")
 //}
 //
-//var objHotel: Hotel?
-//var objApart: Apartament?
+//john.residence = Residence()
 //
-//objHotel = Hotel(name: "Jon Smith ")
-//objApart = Apartament(room: "122")
-//
-//objHotel!.apart = objApart
-//objApart!.hotel = objHotel
-//
-//objApart = nil
-//objHotel = nil
+//if let roomCount = john.residence?.numberOfRooms {
+//    print("John's residence has \(roomCount) room(s).")
+//} else {
+//    print("Unable to retrieve the numbers of rooms.")
+//}
 
-
-//---------------Бесхозная ссылка(unowned)
- 
-//class Country {
 //
+//final class Person {
+//    var residence: Residence?
+//}
+//
+//final class Residence {
+//    var rooms = [Room]()
+//    var numberOfRooms: Int {
+//        return rooms.count
+//    }
+//    subscript(i: Int) -> Room {
+//        get {
+//            return rooms[i]
+//        }
+//        set {
+//            rooms[i] = newValue
+//        }
+//    }
+//    func printNumberOfRooms() {
+//        print("Общее количество комнат равно \(numberOfRooms)")
+//    }
+//    var address: Address?
+//}
+
+//final class Room {
 //    let name: String
-//    var capital: City!
-//
-//    init(name: String, capital: String) {
+//    init(name: String) {
 //        self.name = name
-//        self.capital = City(name: capital, country: self)
 //    }
 //}
 //
-//class City {
-//
-//    let name: String
-//    unowned let country: Country
-//
-//    init(name: String, country: Country) {
-//        self.name = name
-//        self.country = country
+//final class Address {
+//    var buildingName: String?
+//    var buildingNumber: String?
+//    var street: String?
+//    func buildingIdentifier() -> String? {
+//        if let buildingNumber = buildingNumber, let street = street {
+//            return "\(buildingNumber) \(street)"
+//        } else if buildingName != nil {
+//            return buildingName
+//        } else {
+//            return nil
+//        }
 //    }
 //}
 //
-//var country = Country(name: "Russia", capital: "Moscow")
+//let someAddress = Address()
+//someAddress.buildingNumber = "29"
+//someAddress.street = "Acacia Road"
+//john.residence?.address = someAddress
+
+
+//2) Повторить то, что есть в видео.
+
+//final class Adress {
+//    let street = "Тверская"
+//    let number = 18
+//}
+//
+//final class Home {
+//    let adress = Adress()
+//    let room: Int? = 3
+//    var parking: Parking? = Parking()
+//    func printRooms() {
+//        print("The number of rooms is \(room)")
+//    }
+//}
+//
+//struct Parking {
+//    var carPlace = 5
+//}
+//
+//final class Parents {
+//    var cars: [String]? = ["Mercedes"]
+//    var home: Home? = Home()
+//}
+//
+//let parents = Parents()
+//
+//parents.home?.parking?.carPlace
+//
+//if (parents.home?.parking?.carPlace) != nil {
+//    print("Property установлено")
+//} else {
+//    print("Property не установлено")
+//}
+//
+//parents.cars?[0]
+//
+//if (parents.home?.printRooms()) != nil {
+//    print("Комнату уже можно печатать")
+//} else {
+//    print("Комнату нельзя печатать")
+//}
 
 
 /*
- 2 . Описать несколько структур – любой̆ легковой̆ автомобиль и любой̆ грузовик.
- 2.1 Структуры должны содержать марку авто, год выпуска, объем багажника/кузова, запущен ли двигатель, открыты ли окна, заполненный̆ объем багажника.
- 2.2 Описать перечисление с возможными действиями с автомобилем: запустить/заглушить, двигатель, открыть/закрыть окна, погрузить/выгрузить из кузова/багажника груз определенного объема.
- 2.3 Добавить в структуры метод с одним аргументом типа перечисления, который̆ будет менять
- свойства структуры в зависимости от действия.
- 2.4 Инициализировать несколько экземпляров структур. Применить к ним различные действия.
- 2.5 Положить объекты структур в словарь как ключи! названия как строки, например:
- var dict = [structCar: "structCar"]
+ 3) Сделать класс Люди, у класса будут проперти родственники, соседи и т.д. (все опциональные).
+ 3.1) Создать нужно от 15 человек.
+ 3.2) Посчитать, сколько у этого человека двоюродных братьев, троюродных сестёр, теть, дядь, и т.д.
  */
 
-struct Car: Hashable {
+final class People {
     
-    let brand: String
-    let year: Int
-    let trunk: Int
-    var engineCondit: Bool
-    var windowsCondit: Bool
-    let cargo: Int
+    var name = String()
+    var parents: Parents? = Parents()
+    var relatives: Relatives? = Relatives()
+    var neighbours: Neighbours? = Neighbours()
     
-    enum CarActions {
-        case engineOn
-        case engineOff
-        case windowsOpen
-        case windowsClose
-        case loadCargo
-        case unloadCargo
-    }
-    
-    mutating func actions(action: CarActions) {
-        switch action {
-        case .engineOn:
-            engineCondit = true
-            print("The engine is on")
-        case .engineOff:
-            engineCondit = false
-            print("The engine is off")
-        case .windowsOpen:
-            windowsCondit = true
-            print("The windows are open")
-        case .windowsClose:
-            windowsCondit = false
-            print("The windows are closed")
-        case .loadCargo:
-            if cargo != 0 && cargo <= trunk {
-                print("The cargo is loaded")
-            } else if cargo > trunk {
-                print("The cargo is too big")
-            } else {
-                print("There is nothing to load")
-            }
-        case .unloadCargo:
-            if cargo != 0 {
-                print("The cargo is unloaded")
-            } else {
-                print("Trunk is empty")
-            }
-        }
-    }
-    
-    mutating func startEngine(engineOn: CarActions) {
-        engineCondit = true
+    init(name: String) {
+        self.name = name.capitalized
     }
 }
 
-var car = Car(brand: "Maserati", year: 2023, trunk: 580, engineCondit: true, windowsCondit: false, cargo: 500)
-
-struct Truck: Hashable {
+final class Parents {
     
-    let brand: String
-    let year: Int
-    let trunk: Int
-    var engineCondit: Bool
-    var windowsCondit: Bool
-    let cargo: Int
-    
-    enum TruckActions {
-        case engineOn
-        case engineOff
-        case windowsOpen
-        case windowsClose
-        case loadCargo
-        case unloadCargo
-    }
-    
-    mutating func actions(action: TruckActions) {
-        switch action {
-        case .engineOn:
-            engineCondit = true
-            print("The engine is on")
-        case .engineOff:
-            engineCondit = false
-            print("The engine is off")
-        case .windowsOpen:
-            windowsCondit = true
-            print("The windows are open")
-        case .windowsClose:
-            windowsCondit = false
-            print("The windows are closed")
-        case .loadCargo:
-            if cargo != 0 && cargo <= trunk {
-                print("The cargo is loaded")
-            } else if cargo > trunk {
-                print("The cargo is too big")
-            } else {
-                print("There is nothing to load")
-            }
-        case .unloadCargo:
-            if cargo != 0 {
-                print("The cargo is unloaded")
-            } else {
-                print("Trunk is empty")
-            }
-        }
-    }
-    
-    mutating func load(loadCargo: TruckActions) {
-        windowsCondit = false
-    }
+    var parent: [String]? = ["Victor", "Elena"]
 }
 
-var truck = Truck(brand: "Scania", year: 2023, trunk: 10000, engineCondit: false, windowsCondit: true, cargo: 10001)
-
-let transportDict: [AnyHashable: String] = [car: "\(car.brand)", truck: "\(truck.brand)"]
-
+final class Relatives {
     
-//3. Почитать о Capture List (см ссылку ниже) - и описать своими словами и сделать скрин своего примера и объяснения
-
-final class Vehicle {
-    func ride() {
-        print("Ride")
-    }
+    var ucnles: [String]? = ["Evgeniy", "Vitaliy"]
+    var aunts: [String]? = ["Mariya", "Svetlana"]
+    var brother: [String]? = ["Alex"]
+    var sister: [String]? = ["Ann"]
 }
 
-//Сильный захват - используется по умолчанию, захватыевает значение и никогда не даст освободиться.
-
-func rideFast() -> () -> Void {
-    let maserati = Vehicle()
+final class Neighbours {
     
-    let ridingFast = {
-        maserati.ride()
-        return
-    }
-    return ridingFast
+    var neigh: [String]? = ["Bek","Aydana", "Pavel", "Kristina", "Natalia", "Dmitry", "Yana"]
 }
 
-//Слабый захват - всегда optional, не удерживаются и могут быть освобождены и установлены в nil.
+let peo = People(name: "Sergey")
+let par = Parents()
+let rel = Relatives()
+let nei = Neighbours()
 
-func rideNormal() -> () -> Void {
-    let volkswagen = Vehicle()
-    
-    var ridingNormal = { [weak volkswagen] in
-        volkswagen?.ride()
-        return
-    }
-    return ridingNormal
+let countParents = par.parent?.count
+let countUncles = rel.ucnles?.count
+let countAunts = rel.aunts?.count
+let countBrothers = rel.brother?.count
+let countSisters = rel.sister?.count
+let countNeighbours = nei.neigh?.count
+
+if countParents != nil {
+    print("У \(peo.name) \(countParents!) родитель(ей)")
+} else {
+    print("\(peo.name) из детдома:(")
 }
 
-//Безхозная ссылка - альтернатива слабому захвату, но в случае чего приложение упадет как в случае с принудительным извлечением.
+if countUncles != nil {
+    print("У \(peo.name) \(countUncles!) дядя(ь)")
+} else {
+    print("\(peo.name) нет дядь.")
+}
 
-func rideSlow() -> () -> Void {
-    let avtovaz = Vehicle()
-    
-    var ridingSlow = { [unowned avtovaz] in
-        avtovaz.ride()
-        return
-    }
-    return ridingSlow
+if countAunts != nil {
+    print("У \(peo.name) \(countAunts!) тетя(ь)")
+} else {
+    print("\(peo.name) нет теть.")
+}
+
+if countBrothers != nil {
+    print("У \(peo.name) \(countBrothers!) брат(ьев)")
+} else {
+    print("\(peo.name) нет брата(ьев).")
+}
+
+if countSisters != nil {
+    print("У \(peo.name) \(countSisters!) сестра(ер)")
+} else {
+    print("\(peo.name) нет сестры(ер).")
+}
+
+if countNeighbours != nil {
+    print("У \(peo.name) \(countNeighbours!) сосед(ей)")
+} else {
+    print("\(peo.name) нет соседа(ей).")
 }
 
 
-//4. Набрать код на белом скрине, понять в чем там проблема и решить эту проблему
+//4) Создать класс животные и проперти - корова, коза, собака и т.д. Создать класс растения и проперти - трава, цветы и т.д.
 
-final class Cars {
-    var driver: Man?
+final class Animals {
     
-    deinit {
-        print("Машина удалена из памяти")
-    }
+    private let animals: [String] = ["Korova", "Koza", "Sobaka"]
 }
 
-final class Man {
-    weak var myCar: Cars?
+final class Plants {
     
-    deinit {
-        print("Мужчина удален из памяти")
-    }
+    private let plants: [String] = ["Derevo", "Cveti", "Trava"]
 }
 
-var cars: Cars? = Cars()
-var man: Man? = Man()
+//5) Положить все классы массив и отсортировать по алфавиту и по классу - Люди - животные - растения
 
-cars?.driver = man
-man?.myCar = Cars()
+let classes: [Any] = [
+    Parents(),
+    Relatives(),
+    Neighbours(),
+    Animals(),
+    Plants()
+]
 
-cars = nil
-man = nil
+let sortedArray = classes.sorted { $0 is People && $1 is Animals || $0 is Animals && $1 is Plants }
+
+let sortedParrents = classes.filter { $0 is Parents }
+let sortedRelatives = classes.filter { $0 is Relatives }
+let sortedNeighbours = classes.filter { $0 is Neighbours }
+let sortedAnimals = classes.filter { $0 is Animals }
+let sortedPlants = classes.filter { $0 is Plants }
 
 
 /*
-5. У нас есть класс мужчины и его паспорт. Мужчина может родиться и не иметь паспорта, но паспорт выдается конкретному мужчине и не может выдаваться без указания владельца. Чтобы разрешить эту проблему, ссылку на паспорт у мужчины сделаем обычной опциональной, а ссылку на владельца у паспорта – константой. Также добавим паспорту конструктор, чтобы сразу определить его владельца. Таким образом, человек сможет существовать без паспорта, сможет его поменять или выкинуть, но паспорт может быть создан только с конкретным владельцем и никогда не может его сменить. Повторить все, что на черном скрине и решить проблему соблюдая все правила!
-*/
-
-final class Mans {
-    var pasport: Passport?
-    
-    deinit {
-        print("Мужчина удален из памяти")
-    }
-}
-
-final class Passport {
-   unowned let mans: Mans
-    
-    init(mans: Mans) {
-        self.mans = mans
-    }
-    
-    deinit {
-        print("Пасспорт удален из памяти")
-    }
-}
-
-var mans: Mans? = Mans()
-var passport: Passport? = Passport(mans: mans!)
-mans?.pasport = passport
-
-passport = nil
-mans = nil
-
-
-/*
- 6. Создать класс «Ученик». Добавить свойство класса, в котором будет текущая дата. Добавить метод класса, которое выводит текст этого задания в виде многострочного литерала.
+6) Создать 3 класса Rectangle, Circle, Radius
+Класс Квадрат свойства: circle: Circle?
+Класс Radius свойства: radiusName: String
+ Класс Circle свойства: area = [Radius]()
+ 6.1 Для класса Circle добавить вычисляемое свойство, выводящее количество элементов в area
+ 6.2 Для класса Circle добавить сабскрипт
+геттер возвращает запрашиваем элемент
+сеттер добавляет новый элемент в area
+ 6.3 Для класса Circle добавить функцию выводящую количество элементов в area
+ 6.4 Создать метод класса, для каждого из 3 классов, который описывает для чего этот класс предназначен.
+ Создать экземпляры классов.Показать различные опциональные цепочки используя экземпляры классов, их методы и функции.
+ Перед выводом в консоль результатов избавляемся от опционала.
  */
 
-final class Student {
-    var currentDate: Date {
-        return Date.now
-    }
+final class Rectangle {
     
-    func printDate() {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "YYYY:MM:DD HH:MM:SS"
-        
-        print("\(dateFormatter.string(from: currentDate))")
-    }
+    var circle: Circle?
     
-    func printTask() {
-        
-        let tasks = (" Создать класс «Ученик»", "Добавить свойство класса, в котором будет текущая дата", "Добавить метод класса, которое выводит текст этого задания в виде многострочного литерала.")
-        print(tasks.0 + "\n" + tasks.1 + "\n" + tasks.2)
-        
+    static func aboutRectangle() {
+        print("Этот класс описывает квадрат")
     }
 }
 
-let student = Student()
-student.printDate()
-student.printTask()
-
-
-/*
- 7. Создать класс «FlowСontrol» Добавить в класс 5 функций, каждая из которых будет использовать отдельный оператор управления потоком. (В одной массив прогоняется через цикл for, в другой через while, в третьей через repeat while) и т.д.Каждая функция возвращает результат своей работы. Создать enum c кейсами под каждую функцию. Создать экземпляр класса и продемонстрировать работу каждой функции используя значения перечисления.Функции напрямую управления потоком должны быть недоступными для вызова вне класса.
- */
-
-final class FlowControl {
+final class Circle {
     
-    func flowControll(flowType: FlowFunctions, array: [String]?, number: Int?, times: Int?, parameter: inout Int?, method: Car.CarActions?) -> Any {
-          switch flowType {
-          case .forIn: return forIn(array ?? [])
-          case .ifElse: return ifElse(number ?? 0)
-          case .repeatWhile: return repeatWhile(times ?? 0)
-          case .repeatt: return whilee(&parameter)
-          case .switchflow: return switchs(method ?? .engineOn)
-          }
-      }
-
-    private func forIn(_ array: [String]) -> String {
-        var result = String()
-        for string in array {
-            result += string
-        }
-        return result
+    var area = [Radius]()
+    lazy var areaCount = {
+        return self.area.count
     }
-
-    private func whilee(_ parameter: inout Int?) -> Int {
-        guard var parameter = parameter else {
-            return 0
+    
+    subscript(radiusName: String) -> Radius? {
+        get {
+            return area.first { $0.radiusName == radiusName }
         }
-
-        while parameter < 10 {
-            parameter += 1
+        set {
+            area += [Radius(radiusName: radiusName)]
         }
-        return parameter
     }
-
-    private func repeatWhile(_ times: Int) -> String {
-        var result = ""
-        var counter = 0
-        repeat {
-            result += "a"
-            counter += 1
-        } while counter != times
-        return result
+    
+    func printArea() {
+        print("\(area.count) элементов")
     }
-
-    private func ifElse(_ number: Int) -> Int {
-        return number % 2 == 0 ? number : number * 2
-    }
-
-    private func switchs(_ method: Car.CarActions) -> String {
-        switch method {
-        case .engineOn: return "Двигатель работает"
-        case .engineOff: return "Двигатель не работает"
-        default: return "Иное"
-        }
+    
+    static func aboutCircle() {
+        print("Этот класс описывает круг")
     }
 }
 
-enum FlowFunctions {
-    case forIn
-    case repeatt
-    case repeatWhile
-    case ifElse
-    case switchflow
+final class Radius {
+    
+    var radiusName: String
+    
+    init(radiusName: String) {
+        self.radiusName = radiusName
+    }
+    
+    static func aboutRadius() {
+        print("Этот класс описывает радиус")
+    }
 }
 
-let flowController = FlowControl()
+let rectangle = Rectangle()
+let circle = Circle()
+let radius = Radius(radiusName: "Radius N1")
 
-var paramter: Int? = nil
+rectangle.circle = circle
 
-print(flowController.flowControll(flowType: .forIn, array: ["String", "Array"], number: nil, times: nil, parameter: &paramter, method: nil))
+Radius.aboutRadius()
+Rectangle.aboutRectangle()
+Circle.aboutCircle()
+
+rectangle.circle?.areaCount()
+rectangle.circle?.area += [radius]
+
+if let radius = rectangle.circle?["Radius N1"] {
+    print(radius.radiusName)
+}
+
+rectangle.circle?["Radius N2"] = Radius(radiusName: "Radius N2")
+if let radius = rectangle.circle?["Radius N2"] {
+    print(radius.radiusName)
+}
+
