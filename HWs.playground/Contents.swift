3bfb02694b8b7513e3db703c0c4e9c5ce2bac47b
Сергей Ангелов <sergejangelov@MacBook-Air-Sergej.local>
2023-09-14 16:44:00 +0300

    HomeWork23

--- HWs.playground/Contents.swift
+++ HWs.playground/Contents.swift
@@ -1,216 +1,137 @@
 import UIKit
 
-//1) Повторить код из урока.
-//
-//protocol OneProtocol {
-//
-//    var name: String { get set }
-//    var age: String { get set }
-//
-//    //mutating func hello(text: String)
-//}
-//
-//struct Parents: OneProtocol {
-//
-//    var age: String
-//    var name: String
-//}
-//
-//struct Kids: OneProtocol {
-//
-//    var name: String
-//    var age: String
-//}
-//
-//struct Cat: OneProtocol {
-//
-//    var age: String
-//    var name: String
-//}
-//
-//var parents1 = Parents(age: "Jack", name: "28")
-//var parents2 = Parents(age: "Jina", name: "35")
-//var kids = Kids(name: "Masha", age: "5")
-//var cat = Cat(age: "Cat", name: "3")
-//
-//var array: [Any] = [parents1, parents2, cat, kids]
-//
-//for value in array {
-//    if let parent = value as? Parents {
-//        print(parent.name)
-//    } else if let kids = value as? Kids {
-//        print(kids.name)
-//    }
-//}
-//
-//var array1: [OneProtocol] = [parents1, parents2, kids, cat]
-//
-//func sortFamily(array: [OneProtocol]) {
-//    for value in array {
-//        print("\(value.name) - \(value.age)")
-//    }
-//}
-//
-//sortFamily(array: array1)
+//2) Проработать код из видео.
 
+var str = "Делегирование"
+//Delegate
 
-//4) Реализовать структуру IOSCollection и создать в ней copy on write как в уроке - https://youtu.be/QsoqHGgX2rE
+//MARK: - Protocol
 
-struct IosCollection {
-    var integer = 1
+protocol RouterDelegate {
+    var nameID: [String] { get set }
+    func saveName(name: String, text: String)
 }
 
-final class Ref<T> {
-    var value: T
-    init(value: T) {
-        self.value = value
-    }
-}
-
-struct Box<T> {
-    var ref: Ref<T>
+protocol UsersProtocol {
+    var name: String { get set }
+    var age: Int { get set }
+    var delegate: RouterDelegate? { get set }
     
-    init(value: T) {
-        self.ref = Ref(value: value)
-    }
-    var value: T {
-        get {
-            ref.value
-        }
-        set {
-            guard (isKnownUniquelyReferenced(&ref)) else {
-                ref = Ref(value: newValue)
-                return
-            }
-            ref.value = newValue
-        }
+    init(name: String, age: Int, delegate: RouterDelegate?)
+}
+
+//MARK: - Class
+
+class Router: RouterDelegate {
+    var nameID: [String] = []
+    func saveName(name: String, text: String) {
+        nameID.append(name)
+        nameID.append(text)
     }
 }
 
-var int = IosCollection()
-var firstBox = Box(value: int)
-var secondBox = firstBox
-secondBox.value.integer = 2
-
-
-//5) Обьяснить в коментах кратко что такое: a) Copy on write, б) isKnownUniquelyReferenced(), в) strong, weak, unowned ссылки.
-
-/*
- Copy on write(CoW) - оптимизация производительности памяти и ресурсов процессора. Работает она с value - type такими как структуры, массивы, словари. Работает путем создания копий исходных данных при попытки их изменения т.e не потребляя дополнительную память.
- 
- isKnownUniquelyReferenced() - метод используемый для проверки того, является ли объект гарантированно ссылаемым. Если метод возвращает true, значит объект имеет только 1 ссылку и эта ссылка не может быть освобождена, пока существует уникальная ссылка.
- 
- strong - ссылка на объект, которая гарантирует, что объект не будет удален пока на него ссылается хотя бы одна сильная ссылка.
- 
- weak - ссылка на объект, которая указывает, что мы не хотим сохранять объект в памяти, пока на него не будет ссылаться хотя бы одна сильная ссылка
- 
-unowned - ссылка на объект, которая указывает, что мы предполагаем будет существовать в будущем, но не можем гарантировать сильную ссылку на него всегда.
- */
-
-
-//6) Создать протокол переключатель с окончанием «able» как принято в свифт сообществе.
-
-//7) В протокол перключатель добавить метод «переключать».(Все названия только на английском!).
-
-//8) Создать enum Switcher который конформит (conform) протокол переключатель и у которого 2 кейса off, on.
-
-//9) Создать обьект с выбраным кейсом, далее вызвать у обьекта метод переключения который меняет off на on например.
-
-
-protocol Switchable {
-    mutating func toggle()
-}
-
-enum Switcher: Switchable {
+class User: UsersProtocol {
+    var name: String
+    var age: Int
+    var delegate: RouterDelegate?
     
-    case off
-    case on
+    func sendMail(name: String, text: String) -> Bool {
+        delegate?.saveName(name: name, text: text)
+        return true
+    }
     
-    mutating func toggle() {
-        if self == .off {
-            self = .on
-        } else {
-            self = .off
-        }
+    required init(name: String, age: Int, delegate: RouterDelegate?) {
+        self.name = name
+        self.age = age
+        self.delegate = delegate
     }
 }
 
-var switcher = Switcher.on
-switcher.toggle()
+let routerDelegate = Router()
+let user = User(name: "John", age: 23, delegate: routerDelegate)
+user.sendMail(name: "Sergey", text: "Hello Sergey!")
+user.sendMail(name: "Mama", text: "I m OK")
 
+routerDelegate.nameID
 
-//10) Создать протокол Movable с методом run, протокол Shootable с методом shoot.
+//3) Создать протокол BatmanProtocol у которого свойство name.
+//4) Добавить дефолтную реализацию протоколу BatmanProtocol через экстеншен добавить свое имя.
+//5) Создать класс Batman подписатся на протокол BatmanProtocol, создать обьект класса и распечатать name.
 
-protocol Movable {
-    mutating func run()
-}
-
-protocol Shootable {
-    mutating func shoot()
-}
-
-
-//11) Создать протокол HumanProtocol у которого есть свойство name и который конформит протоколы Movable и Shootable.
-
-protocol HumanProtocol: Movable, Shootable {
+protocol BatmanProtocol {
     var name: String { get set }
 }
 
-
-//12) Создать класс Human, подписаться на протокол HumanProtocol и реализовать его, в методы добавить принты.
-
-final class Human: HumanProtocol {
-    var name: String
-    
-    init(name: String) {
-        self.name = name
-    }
-    
-    func run() {
-        print("Я бегу")
-    }
-    
-    func shoot() {
-        print("Я стреляю")
+extension BatmanProtocol {
+    static var defaultName: String {
+        return "Sergey"
     }
 }
 
-var person = Human(name: "Sergey")
-person.run()
-person.shoot()
-person.name
+final class Batman: BatmanProtocol {
+    var name: String = defaultName
+}
+
+let batman = Batman()
+print("Этому городу нужен новый герой \(batman.name)")
+
+
+//6) Создать протокол с 2 мя методами 1 из них должен быть опциональным (optional).
+
+@objc protocol SomeProtocol {
+    
+    func methodOne()
+    @objc optional func methodTwo()
+}
 
 
 /*
- 13) Создать 2 класса и 1 структуру от каждого по одному экзепляру и все это сложить в один массив. Any, AnyObject, Наследование не использовать.
+7) Создать протокол с ассоциативным типом и создать 2 разных класса которые реализуют этот протокол. В 1 м классе 1 тип в другом другой тип.
  */
 
-protocol Family {
-    
+protocol AssociativeTypeProtocol {
+    associatedtype AssocType
+    var value: AssocType { get set }
 }
 
-final class Man: Family {
-    var name: String
-    
-    init(name: String) {
-        self.name = name
+final class FirstClass: AssociativeTypeProtocol {
+    typealias AssType = String
+    var value: String = "Hello"
+}
+final class SecondClass: AssociativeTypeProtocol {
+    typealias AssType = Int
+    var value: Int = 1
+}
+
+
+//8) Создать протокол GoToShopDelegate с методом run.
+//9) Создать класс мама и класс сын. Использовать паттерн delegate что б мама отправляла сына за хлебом.
+
+protocol GoToShopDelegate {
+     func run()
+}
+
+final class Mom: GoToShopDelegate {
+    let son: Son
+
+    init(son: Son) {
+        self.son = son
+    }
+
+    func run() {
+        son.goToShop()
     }
 }
 
-final class Girl: Family {
-    var name: String
-    
-    init(name: String) {
-        self.name = name
+final class Son {
+    var delegate: GoToShopDelegate?
+
+    func goToShop() {
+        delegate?.run()
     }
 }
 
-struct Dog: Family {
-    var name: String
-}
-
-let man = Man(name: "Alex")
-let girl = Girl(name: "Alice")
-let dog = Dog(name: "Charlie")
-
-let arrayFamily: [Family] = [man, girl, dog]
+let mom = Mom(son: Son())
+let son = mom.son
+son.delegate = mom
+son.goToShop()
